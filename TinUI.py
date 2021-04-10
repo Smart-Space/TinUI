267a870d402f8cba3be124aee9d1cab031aa7e8c
Smart-Space <3180471716@qq.com>
2021-04-10 10:39:03 +0800

    Signed-off-by: Smart-Space <3180471716@qq.com>

--- TinUI.py
+++ TinUI.py
@@ -5,7 +5,7 @@ from PIL import Image,ImageTk
 from TinEngine import TinText
 
 class TinUI(Canvas):
-    """基于tkinter的高级绘制组件"""
+    """基于tkinter的高级窗口绘制组件"""
 
     def __init__(self,master,update:bool=True,update_time:int=1000,**kw):
         self.frame = Frame(master)

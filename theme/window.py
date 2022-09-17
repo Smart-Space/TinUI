8b13168aa83467ea83a7a22471de31250b267543
Smart-Space <3180471716@qq.com>
2022-09-17 10:15:26 +0800

    更新
    样式展示窗口添加便捷翻页功能

--- theme/window.py
+++ theme/window.py
@@ -3,6 +3,7 @@ sys.path.append('..')
 from TinUI import *
 
 from tkinter import Tk
+from tkinter.ttk import Button
 
 
 class win:
@@ -14,8 +15,16 @@ class win:
         #gomap=((10,0),(460,0),(460,450),(10,450),(10,0))
         #self.u.create_polygon(gomap,fill='#f0f0f0',outline='#f0f0f0',width=15)
         #self.r.attributes('-transparent','#01FF11')
+        Button(self.r,text='上一页(ctrl+U)',command=self.upit).pack()
+        Button(self.r,text='下一页(ctrl+D)',command=self.downit).pack()
+        self.r.bind('<Control-u>',self.upit)
+        self.r.bind('<Control-d>',self.downit)
     def go(self):
         self.r.mainloop()
+    def upit(self,*e):
+        self.u.yview("scroll", -1, "pages")
+    def downit(self,*e):
+        self.u.yview("scroll", 1, "pages")
 
 
 if __name__=='__main__':

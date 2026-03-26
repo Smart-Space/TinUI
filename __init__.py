6371ca71c738b94049ce29c39f36e9915a059a04
Smart-Space <3180471716@qq.com>
2026-03-26 10:27:19 +0800

    除去extensions支持

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
 from .TinUI import BasicTinUI, TinUI, TinUIXml, TinUITheme, ExpandPanel, VerticalPanel, HorizonPanel
 from .TinUIDialog import show_msg, show_info, show_success, show_warning, show_error, show_question,\
     ask_string, ask_integer, ask_float, ask_choice
-
-from . import extension
